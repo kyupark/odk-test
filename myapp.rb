0a80ec8b2edcffc3c0bb50a0ab69efa223e4bada
Kyu Park <park@kyu.co>
2013-01-31 19:02:39 -0500

    ipn testing

--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require 'net/http'
+require 'rest_client'
 
 get '/' do
   File.read(File.join('public', 'index.html'))
@@ -13,10 +14,14 @@ post '/ipn1' do
   url       = params[:url]
   raw_post  = request.env["rack.input"].read
   res       = Net::HTTP.post_form(URI.parse(url), raw_post)
+  
+  
 end
 
 post '/ipn2' do
   url = "https://www.sandbox.paypal.com/cgi-bin/webscr"
   body = request.body.string
   RestClient.post url, body
+  
+  
 end
