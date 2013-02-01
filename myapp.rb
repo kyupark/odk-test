06dc0c0271cb7b179b2a7af5090f6431fe4f511e
Kyu Park <park@kyu.co>
2013-01-31 22:38:35 -0500

    using slim for donor list

--- myapp.rb
+++ myapp.rb
@@ -1,17 +1,27 @@
 require 'rubygems'
 require 'sinatra'
 require 'rest-client'
+require 'slim'
 
 get '/' do
   File.read(File.join('public', 'index.html'))
 end
 
-configure :production do
+not_found do
+  redirect to('/')
+end
+
+get '/donate' do
+  slim :donate
 end
 
 post '/ipn' do
   url = "https://www.sandbox.paypal.com/cgi-bin/webscr"
   body = request.body.string
   RestClient.post url, body
-  File.open("ipn.txt", 'a') {|f| f.write(body) }
+
+  File.open("paypal_ipn.csv", 'a') {|f| f.puts(body) }
+end
+
+configure :production do
 end
